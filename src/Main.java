0a7df05b4f48b0c4402ae5dc208d49baaa3e02bf
shumelen <shumilin.ilya76@gmail.com>
2022-08-13 21:59:00 +0300

    debug

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         SeaBattle seaBattle = new SeaBattle();
-        int aver = 0;
-        for (int i = 0; i < 5000; i++) {
-            aver += seaBattle.cleverBotTest();
-        }
-        aver /= 5000;
-        System.out.println(aver);
-        System.out.println("Some shit, for example");
+        seaBattle.play();
     }
 }
